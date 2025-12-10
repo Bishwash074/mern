be4b054fe682fefc8f2d45b475d80cd9e7529af6
Bishwash Dhital <bishwashdhital73@gmail.com>
2025-12-10 21:58:23 +0545

    Stop tracking ignored files

--- database/index.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require('mongoose')
-const connectionString="mongodb+srv://bishwasdhital73:V1iZZ5OHoWT2eu6j@mern.kpvay69.mongodb.net/?appName=mern"
-async function connectToDatabase(){
-  await mongoose.connect(connectionString)
-  console.log("Connect to database sucesfully")
-}
-
-module.exports=connectToDatabase
