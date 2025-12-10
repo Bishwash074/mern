33f37f06dc76f22d964d0e8e3a1ad16fdc58920e
Bishwash Dhital <bishwashdhital73@gmail.com>
2025-12-10 20:32:51 +0545

    Initial commit

---
+++ app.js
@@ -0,0 +1,77 @@
+const express=require('express')
+const connectToDatabase = require('./database')
+const Book = require('./modal/bookModal')
+const app=express()
+
+
+app.use(express.json())
+
+
+connectToDatabase()
+app.get("/",(req,res)=>{
+  res.status(200).json({
+    name:"Bishwas",
+    age:20
+  })
+})
+
+app.post("/book",async(req,res)=>{
+  const{bookName,bookPrice,isbnNumber,authorName,publishedAt}=req.body
+  await Book.create({
+    bookName,
+    bookPrice,
+    isbnNumber,
+    authorName,
+    publishedAt,
+  })
+  res.status(201).json({
+    message:"created sucessfully"
+  })
+})
+
+app.get("/book",async(req,res)=>{
+  const book=await Book.find()
+  res.status(200).json({
+    message:"Book fetched sucessfully",
+    data:book
+  })
+})
+
+app.get("/book/:id",async(req,res)=>{
+  const id=req.params.id
+  const book=await Book.findById(id)
+  res.status(200).json({
+    message:"book are fetched sucessfully",
+    data:book
+  })
+})
+
+app.delete("/book/:id",async(req,res)=>{
+  const id=req.params.id
+  const bookDeleted=await Book.findByIdAndDelete(id)
+  res.json({
+    message:"Books sucessfully deleted"
+  })
+})
+
+app.patch("/book/:id",async(req,res)=>{
+  const id =req.params.id
+  const {bookName,bookPrice,isbnNumber,authorName,publishedAt}=req.body
+  const bookUpdate=await Book.findByIdAndUpdate(id,{
+    bookName:bookName,
+    bookPrice:bookPrice,
+    authorName:authorName,
+    isbnNumber:isbnNumber,
+    publishedAt:publishedAt
+
+
+  })
+  res.json({
+    message:"Book Updated Sucessfully"
+  })
+})
+
+app.listen(3000,()=>{
+  console.log("Node js server has started at port 3000")
+})
+
